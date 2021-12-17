89510d3d565d279436548ead426632e9d0698305
Tran Duong Trung <tran.trung@opcbiz.com>
2021-12-17 15:59:17 +0700

    git commit -m "first commit"

---
+++ t/unit/test_models.py
@@ -0,0 +1,122 @@
+import pytest
+
+from datetime import datetime, timedelta
+
+from django.db import transaction
+from django.test import TransactionTestCase
+
+from celery import states, uuid
+
+from django_celery_results.models import TaskResult, GroupResult
+from django_celery_results.utils import now
+
+
+@pytest.mark.usefixtures('depends_on_current_app')
+class test_Models(TransactionTestCase):
+    databases = '__all__'
+
+    @pytest.fixture(autouse=True)
+    def setup_app(self, app):
+        self.app = app
+        self.app.conf.result_serializer = 'pickle'
+        self.app.conf.result_backend = (
+            'django_celery_results.backends:DatabaseBackend')
+
+    def create_task_result(self):
+        id = uuid()
+        taskmeta, created = TaskResult.objects.get_or_create(task_id=id)
+        return taskmeta
+
+    def test_taskmeta(self, ctype='application/json', cenc='utf-8'):
+        m1 = self.create_task_result()
+        m2 = self.create_task_result()
+        m3 = self.create_task_result()
+        assert str(m1).startswith('<Task:')
+        assert m1.task_id
+        assert isinstance(m1.date_done, datetime)
+
+        task = TaskResult.objects.get_task(m1.task_id)
+        assert task._meta.get_field('task_id').max_length == 191
+        assert task.task_id == m1.task_id
+        assert task.status != states.SUCCESS
+        TaskResult.objects.store_result(
+            ctype, cenc, m1.task_id, True, status=states.SUCCESS)
+        TaskResult.objects.store_result(
+            ctype, cenc, m2.task_id, True, status=states.SUCCESS)
+        assert TaskResult.objects.get_task(m1.task_id).status == states.SUCCESS
+        assert TaskResult.objects.get_task(m2.task_id).status == states.SUCCESS
+
+        # Have to avoid save() because it applies the auto_now=True.
+        TaskResult.objects.filter(
+            task_id=m1.task_id
+        ).update(date_done=now() - timedelta(days=10))
+
+        expired = TaskResult.objects.get_all_expired(
+            self.app.conf.result_expires,
+        )
+        assert m1 in expired
+        assert m2 not in expired
+        assert m3 not in expired
+
+        TaskResult.objects.delete_expired(
+            self.app.conf.result_expires,
+        )
+        assert m1 not in TaskResult.objects.all()
+
+    def test_store_result(self, ctype='application/json', cenc='utf-8'):
+        """
+        Test the `using` argument.
+        With the `using` kwarg, we can specify the database to use for updating
+        results. By specifying a different database than the one used by the
+        transaction, we can bypass the transaction and update a result.
+        This is useful for things like progress updates, where a long
+        running transaction has not been committed, but we still want to
+        allow clients to receive updates.
+        """
+        class TransactionError(Exception):
+            pass
+
+        m1 = self.create_task_result()
+        m2 = self.create_task_result()
+        assert set(TaskResult.objects.all()) == set(
+            TaskResult.objects.using("secondary").all()
+        )
+        try:
+            with transaction.atomic():
+                TaskResult.objects.store_result(
+                    ctype, cenc, m1.task_id, True, status=states.SUCCESS)
+                TaskResult.objects.store_result(
+                    ctype, cenc, m2.task_id, True, status=states.SUCCESS,
+                    using='secondary')
+                raise TransactionError()
+        except TransactionError:
+            pass
+
+        assert TaskResult.objects.get_task(m1.task_id).status != states.SUCCESS
+        assert TaskResult.objects.get_task(m2.task_id).status == states.SUCCESS
+
+    def create_group_result(self):
+        id = uuid()
+        taskmeta, created = GroupResult.objects.get_or_create(group_id=id)
+        return taskmeta
+
+    def test_store_group_result(self, ctype='application/json', cenc='utf-8'):
+        class TransactionError(Exception):
+            pass
+
+        m1 = self.create_group_result()
+        m2 = self.create_group_result()
+        assert set(GroupResult.objects.all()) == set(
+            GroupResult.objects.using("secondary").all()
+        )
+
+        try:
+            with transaction.atomic():
+                GroupResult.objects.store_group_result(
+                    ctype, cenc, m1.group_id, True)
+                GroupResult.objects.store_group_result(
+                    ctype, cenc, m2.group_id, True,
+                    using='secondary')
+                raise TransactionError()
+        except TransactionError:
+            pass
---
+++ t/unit/test_views.py
@@ -0,0 +1,113 @@
+import json
+import pytest
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from celery import states, uuid
+from celery.result import GroupResult as CeleryGroupResult, AsyncResult
+
+from django_celery_results.models import TaskResult, GroupResult
+from django_celery_results.views import (
+    is_task_successful,
+    task_status,
+    is_group_successful,
+    group_status
+)
+
+
+@pytest.mark.usefixtures('depends_on_current_app')
+class test_Views(TestCase):
+    @pytest.fixture(autouse=True)
+    def setup_app(self, app):
+        self.app = app
+        self.app.conf.result_serializer = 'json'
+        self.app.conf.result_backend = (
+            'django_celery_results.backends:DatabaseBackend'
+        )
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def create_task_result(self):
+        id = uuid()
+        taskmeta, created = TaskResult.objects.get_or_create(task_id=id)
+        return taskmeta
+
+    def test_is_task_successful(self):
+        taskmeta = self.create_task_result()
+        request = self.factory.get('/done/{}'.format(taskmeta.task_id))
+        response = is_task_successful(request, taskmeta.task_id)
+        assert response
+        result = json.loads(response.content.decode('utf-8'))
+        assert result['task']['executed'] is False
+
+        TaskResult.objects.store_result(
+            'application/json',
+            'utf-8',
+            taskmeta.task_id,
+            json.dumps({'result': True}),
+            status=states.SUCCESS
+        )
+
+        request = self.factory.get('/done/{}'.format(taskmeta.task_id))
+        response = is_task_successful(request, taskmeta.task_id)
+        assert response
+        result = json.loads(response.content.decode('utf-8'))
+        assert result['task']['executed'] is True
+
+    def test_task_status(self):
+        taskmeta = self.create_task_result()
+        request = self.factory.get('/status/{}'.format(taskmeta.task_id))
+        response = task_status(request, taskmeta.task_id)
+        assert response
+        result = json.loads(response.content.decode('utf-8'))
+        assert result['task']['status'] is not states.SUCCESS
+
+        TaskResult.objects.store_result(
+            'application/json',
+            'utf-8',
+            taskmeta.task_id,
+            json.dumps({'result': True}),
+            status=states.SUCCESS
+        )
+
+        request = self.factory.get('/status/{}'.format(taskmeta.task_id))
+        response = task_status(request, taskmeta.task_id)
+        assert response
+        result = json.loads(response.content.decode('utf-8'))
+        assert result['task']['status'] == states.SUCCESS
+
+    def create_group_result(self):
+        """Return a GroupResult model instance
+        with a single, successful result"""
+        id = uuid()
+        task_result = self.create_task_result()
+        task_result.status = states.SUCCESS
+        task_result.save()
+        results = [AsyncResult(id=task_result.task_id)]
+        group = CeleryGroupResult(id=id, results=results)
+        group.save()
+        meta = GroupResult.objects.get(group_id=id)
+        return meta
+
+    def test_is_group_successful(self):
+        meta = self.create_group_result()
+        request = self.factory.get('/group/done/{}'.format(meta.group_id))
+        response = is_group_successful(request, meta.group_id)
+        assert response
+
+        result = json.loads(response.content.decode('utf-8'))
+        assert len(result['group']['results']) == 1
+        result = json.loads(response.content.decode('utf-8'))
+        assert result['group']['results'][0]['executed'] is True
+
+    def test_group_status(self):
+        meta = self.create_group_result()
+        request = self.factory.get('/group/status/{}'.format(meta.group_id))
+        response = group_status(request, meta.group_id)
+        assert response
+
+        result = json.loads(response.content.decode('utf-8'))
+        assert len(result["group"]["results"]) == 1
+        assert result["group"]["results"][0]["status"] == states.SUCCESS
